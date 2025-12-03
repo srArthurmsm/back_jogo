62e0366007444a7538be5a5a9756100ca60a22b5
ARTHUR MARCOS SERPA MARTINS <arthur_m_martins@estudante.sesisenai.org.br>
2025-12-03 08:37:52 -0300

    sync 2

--- sync.js
+++ sync.js
@@ -1,16 +1,15 @@
-const {Cliente, Compra, Cupom, Desenvolvedor,Genero, Jogo, Plataforma, Review} = require('./model/rel')
 const conn = require('./db/conn')
 
-async function SyncDataBase(){
-    try{
-        await conn.sync({force:true})
-    }
-    catch(err){
-        console.error('erro', err)
-    }
-    finally{
-        conn.close()
+async function SyncDataBase() {
+    try {
+        await conn.query('SET FOREIGN_KEY_CHECKS = 0');
+        await conn.sync({ force: true });
+        await conn.query('SET FOREIGN_KEY_CHECKS = 1');
+    } catch (err) {
+        console.error("Erro:", err);
+    } finally {
+        await conn.close();
     }
 }
 
-SyncDataBase()
+SyncDataBase();
